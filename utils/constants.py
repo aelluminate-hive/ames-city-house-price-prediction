4f0d87f679b04b133572485dfafbaa592099f902
noeyislearning <hello@noeyislearning.dev>
2024-10-21 21:51:22 +0800

    feat: add constants for dataset URLs and preprocessed dataset path

---
+++ utils/constants.py
@@ -0,0 +1,5 @@
+TEST_DATASET = "https://gitlab.com/aelluminate/databank/2024-10/ames-city-real-estate/-/raw/main/test.csv"
+TRAIN_DATASET = "https://gitlab.com/aelluminate/databank/2024-10/ames-city-real-estate/-/raw/main/train.csv"
+SAMPLE_SUBMISSION = "https://gitlab.com/aelluminate/databank/2024-10/ames-city-real-estate/-/raw/main/submission.csv"
+
+PREPROCESSED_TRAIN_DATASET = "datasets/preprocessed.csv"
